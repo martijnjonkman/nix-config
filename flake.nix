9c4c37e21ef62b7bdfd6932f3f936c41cfafd08b
martijn <martijnjonkman@hotmail.com>
2026-02-23 22:02:40 +0100

    Initial commit of flake-based NixOS configuration.

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+  {
+    nixosConfigurations = {
+      nixos-btw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
